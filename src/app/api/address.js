f1a8a2ef0a3ee1cd88c37969d197494195939a8e
gumelar <gumelar.wicaksono6@gmail.com>
2023-02-10 00:45:23 +0700

    create fe

---
+++ src/app/api/address.js
@@ -0,0 +1,26 @@
+import { config } from '../../config';
+import axios from 'axios';
+
+export const getAddress = async () => {
+  const { token } = localStorage.getItem('auth') ? JSON.parse(localStorage.getItem('auth')) : {};
+
+  return await axios.get(`${config.api_host}/api/delivery-addresses?limit=''`, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const getLocation = async (lokasi, id) => {
+  return await axios.get(`https://api.goapi.id/v1/regional/${lokasi}?provinsi_id=${id}&api_key=iqSpGeS7eutcnIGNtyu2e6nE4PfCUI`);
+};
+
+export const createAddress = async (data) => {
+  const { token } = localStorage.getItem('auth') ? JSON.parse(localStorage.getItem('auth')) : {};
+
+  return await axios.post(`${config.api_host}/api/delivery-addresses`, data, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
